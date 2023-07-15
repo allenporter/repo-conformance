85fdeb0bf99d05d03906d16407753a7f6ce87574
Allen Porter <allen.porter@gmail.com>
2023-07-14 20:50:17 -0700

    Add pydantic conformance test

--- repo_conformance/checks/__init__.py
+++ repo_conformance/checks/__init__.py
@@ -1,4 +1,4 @@
 """Conformance checks module."""
 
 from . import github, worktree, setupcfg, setuppy, flake8, ruff   # noqa
-from . import renovate, python_version  # noqa
+from . import renovate, python_version, pydantic  # noqa
---
+++ repo_conformance/checks/pydantic.py
@@ -0,0 +1,41 @@
+"""Verify pydantic conformance."""
+
+import configparser
+import logging
+import pathlib
+
+from repo_conformance.exceptions import CheckError
+from repo_conformance.manifest import Repo
+
+from .registries import WORKTREE_CHECKS
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+URL_FORMAT = "https://github.com/{user}/{repo}"
+
+
+@WORKTREE_CHECKS.register(default=False)
+def pydantic(repo: Repo, worktree: pathlib.Path) -> None:
+    """Verify python project setup.cfg conformance."""
+
+    setupcfg = worktree / "setup.cfg"
+    if not setupcfg.exists:
+        raise CheckError("Repo has no setup.cfg")
+
+    config = configparser.ConfigParser()
+    config.read(setupcfg)
+    _LOGGER.debug("setup.cfg: %s", dict(config))
+
+    if "options" not in config:
+        raise CheckError("setup.cfg does not have 'options'")
+    options = config["options"]
+    if "install_requires" not in options:
+        raise CheckError(f"setup.cfg does not have 'options.install_requires': {list(options.items())}")
+    requires = options["install_requires"]
+    if "pydantic" not in requires:
+        raise CheckError(
+            "setup.cfg 'options.install_requires' does not contain 'pydantic': "
+            f"{requires}"
+        )
