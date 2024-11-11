6464da731ee97bd0133b5e5fefb8dc79845458aa
Allen Porter <allen.porter@gmail.com>
2024-11-10 18:58:38 -0800

    Improve spelling

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -5,8 +5,7 @@ from repo_conformance.manifest import parse_manifest
 
 
 def test_parse_manifest() -> None:
-   """Test parsing the current manfiest in the repo."""
+   """Test parsing the current manifest in the repo."""
 
    manifest = parse_manifest()
    assert len(manifest.repos) > 0
-
